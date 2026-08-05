f65be4fc16138d63b5f896c7c9b388bfb0db4635
unknown <abid0001589@gmail.com>
2026-08-06 03:31:41 +0600

    Abid_module-1 feature-1 done

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -15,7 +15,7 @@ function Header(){
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link className="nav-link" to="/">Admin dashboard</Link>
+                                <Link className="nav-link" to="/admin-dashboard">Admin dashboard</Link>
                             </li>
                             <li className="nav-item">
                                 <Link className="nav-link" to="/">Employee dashboard</Link>
